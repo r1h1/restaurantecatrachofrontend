f43dcf53fee8a3124d12512d155d590c8ac24530
r1h1 <de.rivasherrera@gmail.com>
2025-02-24 09:24:09 -0600

    Se agrega metodos GET, POST, PUT, DELETE

--- business/administrative/users.js
+++ business/administrative/users.js
@@ -42,6 +42,17 @@ const makeRequestPostPut = async (url, method, body) => {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
+    // Obtén el token del sessionStorage
+    const token = sessionStorage.getItem("authToken");
+
+    // Agrega el token al encabezado Authorization si existe
+    if (!token) {
+        alert('No se procesó la solicitud, por favor, vuelve a loguearte');
+        return;
+    }
+
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
     const requestOptions = {
         method: method,
         headers: myHeaders,
@@ -87,18 +98,27 @@ const getUsers = async () => {
                 const fila = document.createElement("tr");
 
                 fila.innerHTML = `
-                    <td>${index + 1}</td>
-                    <td>${decodificarBase64(usuario.nombre)}</td>
-                    <td>${decodificarBase64(usuario.correo)}</td>
-                    <td>${decodificarBase64(usuario.rol) == 1 ? 'Administrador' : 'Usuario'}</td>
-                    <td>${decodificarBase64(usuario.telefono)}</td>
-                    <td>
-                        <button class='btn btn-warning btn-sm' onclick='editarUsuario(${usuario.id_usuario})'>Editar</button>
-                        <button class='btn btn-danger btn-sm' onclick='eliminarUsuario(${usuario.id_usuario})'>Eliminar</button>
-                        <button class='btn btn-primary btn-sm' onclick='actualizarContraseña(${usuario.id_usuario})'>Actualizar Clave</button>
-                    </td>
-                `;
-
+                                    <td>${index + 1}</td>
+                                    <td>${decodificarBase64(usuario.nombre)}</td>
+                                    <td>${decodificarBase64(usuario.correo)}</td>
+                                    <td>${decodificarBase64(usuario.rol) == 1 ? 'Administrador' :
+                        decodificarBase64(usuario.rol) == 2 ? 'Gerente' :
+                            decodificarBase64(usuario.rol) == 3 ? 'Empleado' : 'Cliente'}</td>
+                                    <td>${decodificarBase64(usuario.telefono)}</td>
+                                    <td>
+                                        <button class='btn btn-warning btn-sm' 
+                                            onclick='userEdit(
+                                                ${usuario.id_usuario}, 
+                                                "${decodificarBase64(usuario.nombre).replace(/"/g, '&quot;')}", 
+                                                "${decodificarBase64(usuario.correo).replace(/"/g, '&quot;')}", 
+                                                "${decodificarBase64(usuario.rol)}", 
+                                                "${decodificarBase64(usuario.telefono).replace(/"/g, '&quot;')}",
+                                                "${decodificarBase64(usuario.direccion).replace(/"/g, '&quot;')}"
+                                            )'>Editar</button>
+                                        <button class='btn btn-danger btn-sm' onclick='userDelete(${usuario.id_usuario})'>Eliminar</button>
+                                        <button class='btn btn-primary btn-sm' onclick='actualizarContraseña(${usuario.id_usuario})'>Actualizar Clave</button>
+                                    </td>
+                                `;
                 // Agregar la fila al tbody
                 tbody.appendChild(fila);
             });
@@ -109,6 +129,154 @@ const getUsers = async () => {
         alert(error);
     }
 };
+
+
+// Función para borrar usuario de base de datos
+const userDelete = async (id_usuario) => {
+    try {
+        const data = await makeRequestGetDelete(fullApiUrl + '/' + id_usuario, "DELETE");
+
+        if (data.isSuccess === true) {
+            getUsers();
+            alert('Usuario eliminado con éxito');
+        } else {
+            alert('No se pudo eliminar el usuario, verifique nuevamente');
+            getUsers();
+            return;
+        }
+    } catch (error) {
+        alert(error);
+    }
+};
+
+
+//Funcion para editar usuarios
+const userEdit = function (id_usuario, nombre, correo, rol, telefono, direccion) {
+    document.getElementById('id_usuario').value = id_usuario;
+    document.getElementById('nombre').value = nombre;
+    document.getElementById('correo').value = correo;
+    document.getElementById('rol').value = rol;
+    document.getElementById('telefono').value = telefono;
+    document.getElementById('direccion').value = direccion;
+}
+
+// Función para traer datos de usuario logueado y llenar la tabla
+const userUpdate = async () => {
+    const id_usuario = document.getElementById('id_usuario').value;
+    const nombre = document.getElementById('nombre').value;
+    const correo = document.getElementById('correo').value;
+    const rol = document.getElementById('rol').value;
+    const telefono = document.getElementById('telefono').value;
+    const direccion = document.getElementById('direccion').value;
+
+    try {
+
+        const body = {
+            id_usuario,
+            nombre,
+            correo,
+            rol,
+            telefono,
+            direccion
+        };
+
+        const data = await makeRequestPostPut(fullApiUrl, "PUT", body);
+
+        if (data.isSuccess === true) {
+            getUsers();
+            alert('Usuario actualizado con éxito');
+            resetData();
+        } else {
+            alert("No se pudo actualizar, verifique de nuevo");
+            getUsers();
+            return;
+        }
+    } catch (error) {
+        alert(error);
+    }
+};
+
+
+// Función para traer datos de usuario logueado y llenar la tabla
+const userCreate = async () => {
+    const id_usuario = 0;
+    const nombre = document.getElementById('nombre').value;
+    const correo = document.getElementById('correo').value;
+    const rol = document.getElementById('rol').value;
+    const telefono = document.getElementById('telefono').value;
+    const direccion = document.getElementById('direccion').value;
+    const clave = document.getElementById('clave').value;
+
+    if(!nombre || !correo || !rol || !telefono || !direccion || !clave){
+        alert('Todos los datos son obligatorios para crear un usuario.');
+        return;
+    }
+
+    try {
+
+        const body = {
+            id_usuario,
+            nombre,
+            correo,
+            clave,
+            rol,
+            telefono,
+            direccion
+        };
+
+        const data = await makeRequestPostPut(fullApiUrl, "POST", body);
+
+        if (data.isSuccess === true) {
+            getUsers();
+            alert('Usuario creado con éxito');
+            resetData();
+        } else {
+            alert("No se pudo crear, verifique de nuevo");
+            getUsers();
+            return;
+        }
+    } catch (error) {
+        alert(error);
+    }
+};
+
+
+//Funcion para el botón guardar, cuando exista algo en ID usuario va a editar, cuando no, crear
+const saveButtonOptions = function () {
+    const option = document.getElementById('id_usuario').value;
+    const clave = document.getElementById('clave').value;
+    try {
+        if (option) {
+            userUpdate();
+            getUsers();
+        }
+        else {
+            if (clave) {
+                userCreate();
+                getUsers();
+            }
+            else {
+                return;
+            }
+        }
+    }
+    catch (error) {
+        alert(error);
+        return;
+    }
+}
+
+
+//Funcion para borrar datos
+const resetData = function(){
+    document.getElementById('id_usuario').value = "";
+    document.getElementById('nombre').value = "";
+    document.getElementById('correo').value = "";
+    document.getElementById('rol').value = 1
+    document.getElementById('telefono').value = "";
+    document.getElementById('direccion').value = "";
+    document.getElementById('clave').value = "";
+}
 
 
 getUsers();
--- business/users/profile.js
+++ business/users/profile.js
@@ -58,3 +58,5 @@ const getProfile = async () => {
         alert(error);
     }
 };
+
+getProfile();
